cf178351a2460975cb57ec70dc92b2eabcda70d9
VrushabhChaudhari24 <vrushabhchaudhari@virtualemployee.com>
2026-01-14 14:33:59 +0530

    only add dotenv variable

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    mongoose.set('bufferCommands', false); // Disable buffering
- 
-    await mongoose.connect('mongodb+srv://vrushabhchaudhari_db_user:KL32y1jyHgc3Brtq@cluster0.4jnq7rq.mongodb.net/?appName=Cluster0', {
-      maxPoolSize: 50,              // Adjust based on pod count
-      minPoolSize: 10,
-      serverSelectionTimeoutMS: 10000,
-      socketTimeoutMS: 45000,
-      connectTimeoutMS: 10000,
-      retryWrites: true,
-      readPreference: 'secondaryPreferred', // If replica exists
-    });
- 
-    console.log('MongoDB connected successfully');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection failed", error);
     process.exit(1);
   }
 };
