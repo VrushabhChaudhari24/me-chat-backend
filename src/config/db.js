de7afa50057ed85c75c0a473d2c10e52603ef756
VrushabhChaudhari24 <vrushabhchaudhari@virtualemployee.com>
2026-01-14 13:47:58 +0530

    update db connection file

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,21 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      maxPoolSize: 50,
+    mongoose.set('bufferCommands', false); // Disable buffering
+ 
+    await mongoose.connect('mongodb+srv://vrushabhchaudhari_db_user:KL32y1jyHgc3Brtq@cluster0.4jnq7rq.mongodb.net/?appName=Cluster0', {
+      maxPoolSize: 50,              // Adjust based on pod count
       minPoolSize: 10,
       serverSelectionTimeoutMS: 10000,
       socketTimeoutMS: 45000,
+      connectTimeoutMS: 10000,
+      retryWrites: true,
+      readPreference: 'secondaryPreferred', // If replica exists
     });
-    console.log("MongoDB connected");
+ 
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error("MongoDB connection failed", error);
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
